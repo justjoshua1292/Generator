ee5b0321f9f7cca1d96c08ef785cd134d48ac035
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-11 12:00:31 -0400

    working on index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const employee = require("./Tests/employee");
+const engineer = require("./Tests/engineer");
+const intern = require("./Tests/intern");
+const manager = require ("./Tests/manager");
