7a92ad98415894c0d12342ac50cbecd3041834a1
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-11 22:09:45 -0400

    renamed files in directory, found a few errors to correct

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const employee = require("./Tests/employee");
-const engineer = require("./Tests/engineer");
-const intern = require("./Tests/intern");
-const manager = require ("./Tests/manager");
+const employee = require("./lib/employee");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+const manager = require ("./lib/manager");
