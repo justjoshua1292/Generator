cdab43304fe0853fa5c20bbbd85e12f06ebda5be
Joshua Harris <Justjoshua1292@gmail.com>
2021-08-25 17:53:06 -0400

    adding to template.js file

--- src/template.js
+++ src/template.js
@@ -7,6 +7,37 @@ const template = `<!DOCTYPE html>
     <title>Document</title>
 </head>
 <body>
-    <h2>${manager.name}</h2>
+<div class="d-flex">
+    <div class="card m-3" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${manager.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+        <p class="card-text">ID: ${manager.id}</p>
+        <p class="card-text">Email: ${manager.email}</p>
+        <p class="card-text">Office Number: ${manager.officeNumber}.</p>
+        </div>
+    </div>
+
+    <div class="card m-3" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${intern.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Intern</h6>
+        <p class="card-text">ID: ${intern.id}</p>
+        <p class="card-text">Email: ${intern.email}</p>
+        <p class="card-text">School: ${intern.school}.</p>
+        </div>
+    </div>
+
+
+    <div class="card m-3" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${engineer.name}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Engineer</h6>
+        <p class="card-text">ID: ${engineer.id}</p>
+        <p class="card-text">Email: ${engineer.email}</p>
+        <p class="card-text">Github: ${engineer.github}.</p>
+        </div>
+    </div>
+</div>
 </body>
 </html>`
